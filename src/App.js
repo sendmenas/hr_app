eadcdea121937dadf52cc9fb14a7d90d75e68ee6
Giedrius Bielskis <giedrius.bielskis@gmail.com>
2019-06-06 21:22:39 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import data from './data.json'
+
+import DataList from './components/DataList'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      candidates: [],
+      selected: 0
+    }
+  }
+
+  componentWillMount() {
+    const candidates = data.data;
+    this.setState(candidates)
+    console.log(candidates)
+    console.log(this.state)
+  }
+
+  handleCheck(id, val) {
+    const candidates = this.state.candidates.slice()
+    candidates[id].hired = val;
+    this.setState(candidates);
+  }
+
+  render () {
+    return (
+      <main>
+        <section>
+          <DataList
+            data={this.state.candidates[0]}
+            handleCheck={(evt) => this.handleCheck(0, evt.target.checked)}
+          />
+        </section>
+      </main>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/DataList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function DataList(props) {
+    return (
+        <section>
+            <picture>
+                <img/>
+            </picture>
+            <dl>
+                <dt>Name:</dt>
+                <dd>{props.data.name}</dd>
+                <dt>BOD:</dt>
+                <dd>{props.data.bod}</dd>
+            </dl>
+            <label for="hired">Hired:</label>
+            <input
+                id="hired"
+                type="checkbox"
+                selected={props.data.hired}
+                onInput={props.handleCheck}
+            />
+        </section>
+    )
+}
+
+export default DataList
