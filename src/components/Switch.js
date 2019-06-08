9561349b8ba74773adfca2b559e08b5e025004f4
sendmenas <giedrius.bielskis@gmail.com>
2019-06-08 22:12:51 +0300

    add navigation

---
+++ src/components/Switch.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Switch = ({ direction, clickHandler }) => (
+	<button className={`switch switch--${direction}`} onClick={clickHandler}>{direction}</button>
+)
+
+Switch.propTypes = {
+	direction: PropTypes.string.isRequired,
+	clickHandler: PropTypes.func.isRequired
+}
+
+export default Switch
