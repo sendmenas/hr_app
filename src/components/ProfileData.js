ab060405154a7d505fdbe3bbd4a04bac521b63aa
Giedrius Bielskis <giedrius.bielskis@gmail.com>
2019-06-25 15:08:43 +0300

    fix spacing

--- src/components/ProfileData.js
+++ src/components/ProfileData.js
@@ -3,31 +3,31 @@ import PropTypes from 'prop-types'
 import './ProfileData.scss'
 
 const ProfileData = ({ data, handleCheck }) => (
-		<section>
-				<picture className="picture">
-						<img src="" alt="" />
-				</picture>
-					<ul className="list">
-						<li className="list__row">
-							<div className="list__row__title">Name:</div>
-							<div className="list__row__data">{data.name}</div>
-						</li>
-						<li className="list__row">
-							<div className="list__row__title">BOD:</div>
-							<div className="list__row__data">{data.bod}</div>
-						</li>
-						<li className="list__row">
-							<label className="list__row__title" htmlFor="hired">Hired:</label>
-							<input
-								className="list__row__data"
-								id="hired"
-								type="checkbox"
-								selected={data.hired}
-								onInput={handleCheck}
-							/>
-						</li>
-				</ul>
-		</section>
+	<section>
+			<picture className="picture">
+					<img src="" alt="" />
+			</picture>
+				<ul className="list">
+					<li className="list__row">
+						<div className="list__row__title">Name:</div>
+						<div className="list__row__data">{data.name}</div>
+					</li>
+					<li className="list__row">
+						<div className="list__row__title">BOD:</div>
+						<div className="list__row__data">{data.bod}</div>
+					</li>
+					<li className="list__row">
+						<label className="list__row__title" htmlFor="hired">Hired:</label>
+						<input
+							className="list__row__data"
+							id="hired"
+							type="checkbox"
+							selected={data.hired}
+							onInput={handleCheck}
+						/>
+					</li>
+			</ul>
+	</section>
 )
 
 ProfileData.propTypes = {
