9a8905c6f28bf0d09ce38940e0d6fa8f3a35e3d9
sendmenas <giedrius.bielskis@gmail.com>
2019-07-20 23:26:08 +0300

    update css

--- src/components/ProfileData.js
+++ src/components/ProfileData.js
@@ -3,25 +3,17 @@ import PropTypes from 'prop-types'
 import './ProfileData.scss'
 
 const ProfileData = ({ data, handleCheck }) => {
-	console.log(data)
 	return (
-	<section>
+	<section className="profile-data">
 			<picture className="picture">
 					<img src={data.avatar} alt="avatar" />
 			</picture>
 				<ul className="list">
-					<li className="list__row">
-						<div className="list__row__title">Name:</div>
-						<div className="list__row__data">{data.name} {data.surname}</div>
-					</li>
-					<li className="list__row">
-						<div className="list__row__title">BOD:</div>
-						<div className="list__row__data">{data.dob}</div>
-					</li>
-					<li className="list__row">
-						<label className="list__row__title" htmlFor="hired">Hired:</label>
+					<li className="list__row">Name: {data.name} {data.surname}</li>
+					<li className="list__row">BOD: {data.dob}</li>
+					<li className="list__row">Hired:
 						<input
-							className="list__row__data"
+							className="list__row__checkbox"
 							id="hired"
 							type="checkbox"
 							checked={data.hired}
--- src/components/Switch.js
+++ src/components/Switch.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import './Switch.scss'
 
 const Switch = ({ direction, clickHandler, disabled }) => (
-	<button className={`switch switch--${direction}`} onClick={clickHandler} disabled={disabled}>{direction}</button>
+	<button className={`switch switch--${direction}`} onClick={clickHandler} disabled={disabled}></button>
 )
 
 Switch.propTypes = {
